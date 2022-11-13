651dd6a44b6fac3ad5a61f4e84353d678dc895d4
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-11-13 12:04:24 +0000

    Load image

---
+++ Loading and Displaying Images/opencv-load-image/opencv-load-image/load_image_opencv.py
@@ -0,0 +1,33 @@
+# USAGE
+# python load_image_opencv.py --image sad64.jpg
+
+import argparse
+
+import cv2
+# import the necessary packages
+from matplotlib import pyplot as plt
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="D:\Github_Desktop\Computer-vision-practice\Loading and Displaying Images\opencv-load-image\opencv-load-image\sad64.jpg")
+args = vars(ap.parse_args())
+
+# load the image from disk via "cv2.imread" and then grab the spatial
+# dimensions, including width, height, and number of channels
+image = cv2.imread(args["image"])
+(h, w, c) = image.shape[:3]
+
+# display the image width, height, and number of channels to our
+# terminal
+print("width: {} pixels".format(w))
+print("height: {}  pixels".format(h))
+print("channels: {}".format(c))
+
+# show the image and wait for a keypress
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# save the image back to disk (OpenCV handles converting image
+# filetypes automatically)
+cv2.imwrite("newimage.jpg", image)
