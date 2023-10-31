f41abd3d89c7f93bb7e17206cc98eac38504c495
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2023-10-31 14:48:28 +0000

    added

--- smoothing-and-blurring/bilateral.py
+++ smoothing-and-blurring/bilateral.py
@@ -7,7 +7,7 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, default="adrian.png",
+ap.add_argument("-i", "--image", type=str, default="sad64.jpg",
 	help="path to input image")
 args = vars(ap.parse_args())
 
